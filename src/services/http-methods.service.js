4e766a2143254ddf1dc7fb0f0138312f0277beb9
Daniel Kalbasi <danial.kalbassi@gmail.com>
2018-04-07 11:09:46 +0800

    Add http-methods service
    - Add http-methods service
    - Add it's unit test

---
+++ src/services/http-methods.service.js
@@ -0,0 +1,104 @@
+/**
+ * This service handles http methods operation
+ */
+export default class HttpMethodsService {
+    /**
+     * keep the http headers
+     */
+    headers = {};
+
+    /**
+     * Cache the http response
+     */
+    caches = [];
+
+    /**
+     * Set the default headers
+     */
+    constructor() {
+        this.setDefaultHeaders();
+    }
+
+    /**
+     * Call the get http method
+     * If the URL has cache data, it will return it's data
+     * otherwise it will do a new call
+     * @param {*} url is the url of endpoint
+     * @param {*} cache whether should be cached
+     * @param {*} headers is the optional header
+     */
+    get(url, cache = false, headers) {
+        const getHeaders = headers || this.headers;
+
+        if (cache) {
+            const urlkey = this.getUrlKey(url);
+            const cached = this.caches[urlkey];
+
+            if (cached) {
+                const responsePromise = new Promise((resolve) => {
+                    resolve(cached);
+                });
+                return responsePromise;
+            }
+            return this
+                .baseGet(url, getHeaders)
+                .then(this.toJson)
+                .catch(error => Promise.reject(this.handleError(url, error)));
+        }
+        
+        return this
+            .baseGet(url, getHeaders)
+            .then(this.toJson)
+            .catch(error => Promise.reject(this.handleError(url, error)));
+    }
+
+    baseGet(url, headers) {
+        const header = { headers: headers };
+
+        return fetch(url, header).then((result) => {
+            if (result.ok) {
+                return result;
+            }
+            return Promise.reject(this.handleError(url, JSON.stringify(result)));
+        }).catch(error => Promise.reject(this.handleError(url, error)));
+    }
+
+    /**
+     * Handle http errors
+     * @param {*} url is the request url
+     * @param {*} error is the error object
+     */
+    handleError(url, error) {
+        const errorMessage = `The URL: ${url}, Error: ${error}`;
+        throw new Error(errorMessage);
+    }
+
+    /**
+     * Set default header object
+     */
+    setDefaultHeaders() {
+        this.headers = new Headers({
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        });
+    }
+
+    /**
+     * Convert response data to the arra/object
+     * if the data is not json, then it iwll return the data itself
+     * @param {*} data is the response data
+     */
+    toJson(data) {
+        const isResponse = data instanceof Response;
+        return isResponse ? data.json() : data;
+    }
+
+    /**
+     * Generate a unique key based on the endpoint url
+     * The url use as a key to retrieve a cache data for specific request
+     * @param {*} url is the endpoint url
+     */
+    getUrlKey(url) {
+        return url ? url.replace(/[^\w\s]/gi, '') : '';
+    }
+}
---
+++ src/services/http-methods.service.test.js
@@ -0,0 +1,139 @@
+import HttpMethodsService from './http-methods.service';
+
+describe('HttpMethodsService', () => {
+    let service;
+    let responseData = JSON.stringify({
+        "products": [
+            { "Name": "Cheese", "Price": 2.50, "Location": "Refrigerated foods" },
+            { "Name": "Crisps", "Price": 3, "Location": "the Snack isle" },
+        ]
+    });
+    const responseInit = { "status": 200, "statusText": "success!" };
+
+    beforeEach(() => {
+        service = new HttpMethodsService();
+    });
+
+    describe('Initialization', () => {
+        it('should set the default headers', () => {
+            // Arrange
+            const header = new Headers({
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            });
+
+            // Act
+            const headers = service.headers;
+
+            // Assert
+            expect(service.headers).toEqual(header);
+        });
+    });
+
+    describe('get', () => {
+        let callOnServiceGet;
+        let spyOnBaseGet;
+
+        beforeEach(() => {
+            spyOnBaseGet = spyOn(service, 'baseGet')
+                .and.returnValue(Promise.resolve(false));
+            callOnServiceGet = service.get('url', false, null).then(() => { });
+        });
+
+        it('should call baseGet if the cached is false', () => {
+            // Assert
+            expect(spyOnBaseGet).toHaveBeenCalled();
+        });
+
+        it('should return promise if the cache is false', () => {
+            // Assert
+            expect(callOnServiceGet instanceof Promise).toBeTruthy();
+        });
+
+        it('should return promise if the cache is true', () => {
+            // Act
+            callOnServiceGet = service.get('url', true, null).then(() => { });
+
+            // Assert
+            expect(callOnServiceGet instanceof Promise).toBeTruthy();
+        });
+    });
+
+    describe('setDefaultHeaders', () => {
+        it('should set the default header', () => {
+            // Arrange
+            const header = new Headers({
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            });
+
+            // Act
+            service.setDefaultHeaders();
+
+            // Assert
+            expect(service.headers).toEqual(header);
+        })
+    });
+
+    describe('toJson', () => {
+        it('should return parameter value if the parameter is not Response object', () => {
+            // Arrange
+            let param = false;
+
+            // Act
+            const result = service.toJson(param);
+
+            // Assert
+            expect(result).toBeFalsy();
+        });
+
+        it('should return promise if the response is valid', () => {
+            // Arrange
+            let param = new Response(responseData, responseInit);
+
+            // Act
+            const result = service.toJson(param);
+
+            // Assert
+            expect(result instanceof Promise).toBeTruthy();
+        });
+
+        it('should transform json to js object', () => {
+            // Arrange
+            let param = new Response(responseData, responseInit);
+            let jsonParse = JSON.parse(responseData);
+
+            // Act 
+            // Assert
+            const result = service.toJson(param).then(result => {
+                expect(jsonParse).toEqual(result);
+            });
+        });
+    });
+
+    describe('getUrlKey', () => {
+        it('should return a key from a url', () => {
+            // Arrange
+            const url = 'http://getme.com';
+
+            // Act
+            const result = service.getUrlKey(url);
+            const manualResult = url.replace(/[^\w\s]/gi, '');
+
+            // Assert
+            expect(result).toEqual(manualResult);
+        });
+
+        it('should return empty string if url is undefined or null or empty', () => {
+            // Arrange
+            const url = undefined;
+
+            // Act
+            const result = service.getUrlKey(url);
+
+            // Assert
+            expect(result).toBe('');
+            expect(result).toBeDefined();
+        });
+    });
+});
